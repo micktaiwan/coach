79e4083e279dbaac7544c60bd13288862a183416
Mickael <mickael@lempire.co>
2023-05-08 17:39:28 +0200

    improve button

--- imports/ui/chats/chats.js
+++ imports/ui/chats/chats.js
@@ -45,5 +45,20 @@ Template.chats.events({
     Meteor.call('deleteChat', chatId, (err, res) => {
       if (err) alert(err);
     });
-  }
+  },
+
+  'click .js-improve-answer'(event, instance) {
+    event.preventDefault();
+    const message = "Let's work this out in a step by step way to be sure we have the right answer.";
+    Meteor.call('addChat', Session.get('contextId'), 'user', message, (err, res) => {
+      Meteor.call('openaiGenerateText', Session.get('contextId'), '', '', (err, res) => {
+        if(err) {
+          console.log(err);
+          Meteor.call('addChat', Session.get('contextId'), 'meta', err.reason.response.data.error.message);
+        }
+        else  Meteor.call('addChat', Session.get('contextId'), 'assistant', res);
+      });
+    });
+
+  },
 });
