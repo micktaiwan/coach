9ec1b51156b69dcc7a30ff7e4630828f441fcc1a
Mickael <mickael@lempire.co>
2023-05-09 20:07:28 +0200

    updating gitignore

--- .prod/backupdb.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh -t root@91.134.136.54 'export LC_ALL=C;mongodump --port 27017'
--- .prod/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-date
-mup deploy
-#ssh -t root@91.134.136.54 'chown -R meteoruser:root /home/mick/ob-medias/'
-# cp -r /home/mick/codeorbiter/public/* /opt/codeOrbiter/app/programs/web.browser/app;
