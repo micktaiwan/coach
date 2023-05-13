ea18969f4640d2763a6e205e0f790f5e01e5efca
Mickael Faivre-Maçon <faivrem@gmail.com>
2023-05-14 00:14:42 +0200

    going back to Kadira

--- imports/api/primary-contexts/publish.js
+++ imports/api/primary-contexts/publish.js
@@ -1,6 +1,5 @@
 import { PrimaryContexts } from './collections.js';
 
 Meteor.publish('primary_contexts', function(contextId, dynContextId) {
-  // console.log('publishing primary_contexts', contextId, dynContextId || { $exists: false });
   return PrimaryContexts.find({ contextId, userId: this.userId, dynContextId: dynContextId || { $exists: false } });
 });
