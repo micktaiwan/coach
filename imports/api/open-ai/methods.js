e602574e4856f08c817842d6288e1282ea59bfca
Mickael Faivre-Maçon <faivrem@gmail.com>
2023-05-20 18:25:27 +0200

    adding contexts to pinecone

--- imports/api/open-ai/methods.js
+++ imports/api/open-ai/methods.js
@@ -12,40 +12,40 @@ const todayAsString = () => {
   return today.toLocaleDateString(undefined, options);
 };
 
-const getSystem = (contextId, dynContextIds) => {
-  let tasks;
-  if (dynContextIds.includes('tasks')) {
-    tasks = Tasks.find({ contextId }, { sort: { priority: 1 } }).map(task => `[${task.priority + 1}] ${
-      task.text
-    } [status: ${task.status}]` +
-    ` [createdAt: ${task.createdAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]${
-      task.startedAt ? ` [startedAt: ${task.startedAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]` : ''
-    }${task.doneAt ? ` [doneAt: ${task.doneAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]` : ''}`,
-    ).join(', ');
-    dynContextIds = _.without(dynContextIds, 'tasks');
-  }
+// const getSystem = (contextId, dynContextIds) => {
+//   let tasks;
+//   if (dynContextIds.includes('tasks')) {
+//     tasks = Tasks.find({ contextId }, { sort: { priority: 1 } }).map(task => `[${task.priority + 1}] ${
+//       task.text
+//     } [status: ${task.status}]` +
+//     ` [createdAt: ${task.createdAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]${
+//       task.startedAt ? ` [startedAt: ${task.startedAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]` : ''
+//     }${task.doneAt ? ` [doneAt: ${task.doneAt.toLocaleDateString(undefined, { year: '2-digit', month: 'short', day: 'numeric' })}]` : ''}`,
+//     ).join(', ');
+//     dynContextIds = _.without(dynContextIds, 'tasks');
+//   }
 
-  const primaryContexts = PrimaryContexts.find({ contextId, dynContextId: { $exists: false } }, { sort: { priority: 1 } }).fetch();
-  for (const id in dynContextIds) {
-    const contexts = PrimaryContexts.find({ dynContextId: dynContextIds[id] }).fetch();
-    if (contexts) primaryContexts.push(...contexts);
-  }
+//   const primaryContexts = PrimaryContexts.find({ contextId, dynContextId: { $exists: false } }, { sort: { priority: 1 } }).fetch();
+//   for (const id in dynContextIds) {
+//     const contexts = PrimaryContexts.find({ dynContextId: dynContextIds[id] }).fetch();
+//     if (contexts) primaryContexts.push(...contexts);
+//   }
 
-  const context = primaryContexts.map(context => context.text).join('\n');
+//   const context = primaryContexts.map(context => context.text).join('\n');
 
-  let system = `Act as a coach. You have 30+ years of experience.\n` +
-  `Today is ${todayAsString()}.\n` +
-  `Assume that the user is your client.\n` +
-  `Any first person pronoun is your client speaking about himself.\n` +
-  `This is all the context your client gave you:\n${
-    context}\n[end of context]\n` +
-  `This context is very important and must be taken into account for each reply you provide to your client.\n` +
-  `Always reply in the same language as the last chat:\n`;
+//   let system = `Act as a coach. You have 30+ years of experience.\n` +
+//   `Today is ${todayAsString()}.\n` +
+//   `Assume that the user is your client.\n` +
+//   `Any first person pronoun is your client speaking about himself.\n` +
+//   `This is all the context your client gave you:\n${
+//     context}\n[end of context]\n` +
+//   `This context is very important and must be taken into account for each reply you provide to your client.\n` +
+//   `Always reply in the same language as the last chat:\n`;
 
-  if (tasks) system += `This is the list of your client current tasks:"${tasks}"`;
+//   if (tasks) system += `This is the list of your client current tasks:"${tasks}"`;
 
-  return system;
-};
+//   return system;
+// };
 
 const getPineconeSystem = async (contextId, prompt) => {
   const context = await pinecone.getContext(contextId, prompt);
@@ -75,9 +75,10 @@ Meteor.methods({
     const model = Meteor.user()?.openAI?.model || 'gpt-3.5-turbo';
     if (!apiKey) throw new Meteor.Error('no-api-key', 'No OpenAI API key found');
 
-    if (system === '' && contextId) system = await getPineconeSystem(contextId, prompt);
-
     const pastChats = Chats.find({ contextId, role: { $ne: 'meta' } }, { sort: { createdAt: 1 }, projection: { _id: 0, role: 1, content: 1 } }).fetch();
+    const lastPrompt = prompt || pastChats[pastChats.length - 1].content;
+    if (system === '' && contextId) system = await getPineconeSystem(contextId, lastPrompt);
+
     const messages = [
       { role: 'system', content: system },
       ...pastChats,
@@ -85,7 +86,7 @@ Meteor.methods({
 
     if (prompt) messages.push({ role: 'user', content: prompt });
 
-    console.log('prompt:', prompt);
+    console.log('prompt:', lastPrompt);
     console.log('messages:', messages);
 
     const data = {
