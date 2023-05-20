51a77f3a04ee476a6dd83884444f3f9e7d3ce9a7
Mickael <mickael@lempire.co>
2023-05-20 21:07:37 +0200

    some fixes

--- imports/api/open-ai/publish.js
+++ imports/api/open-ai/publish.js
@@ -1,5 +1,6 @@
-import { UsageStats } from './collections.js';
+import { UsageStats } from './collections';
 
 Meteor.publish('usage_stats', function(contextId) {
+  check(contextId, String);
   return UsageStats.find({ contextId, userId: this.userId }, { sort: { createdAt: -1 }, limit: 1 });
 });
